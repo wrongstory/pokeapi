8748594aa6a4240599ee565b03c2915bc72681f4
L <lmwoo0327@naver.com>
2025-06-12 10:41:15 +0900

    fetch #1. API fetch 확인

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
+import { useEffect } from "react";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    const fetchAPI = async () => {
+      const response = await fetch("https://pokeapi.co/api/v2/");
+      const data = await response.json();
+      console.log(data);
+    };
+    fetchAPI();
+  }, []);
   return <></>;
 }
 
