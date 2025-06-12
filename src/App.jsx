0a7f7ca82770d62b4020b3c76995bd0e591de88d
L <lmwoo0327@naver.com>
2025-06-12 15:04:59 +0900

    fetch #8. 페이지 라우팅

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import { useEffect } from "react";
 import "./App.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchMultiplePokemonById } from "./RTK/thunk";
+import { Link, Route, Routes } from "react-router-dom";
+import Main from "./pages/Main";
+import Detail from "./pages/Detail";
+import Search from "./pages/Search";
+import Favorite from "./pages/Favorite";
 
 function App() {
   const dispatch = useDispatch();
@@ -14,6 +19,20 @@ function App() {
   return (
     <>
       <h1 className="text-[40px] text-center">포켓몬 도감</h1>
+      <nav className="flex gap-[10px] justify-center">
+        <Link to={"/"}>메인</Link>
+        <Link to={"detail/1"}>상세정보</Link>
+        <Link to={"search"}>검색</Link>
+        <Link to={"favorite"}>찜목록</Link>
+      </nav>
+      <main className="flex justify-center">
+        <Routes>
+          <Route path={"/"} element={<Main />} />
+          <Route path={"/detail/:pokemonId"} element={<Detail />} />
+          <Route path={"/search"} element={<Search />} />
+          <Route path={"/favorite"} element={<Favorite />} />
+        </Routes>
+      </main>
     </>
   );
 }
