1a009e79a5e644d593f9e26afffa95d5923fea59
L <lmwoo0327@naver.com>
2025-06-12 16:42:13 +0900

    fetch #10. Search 탭 생성 및 url 설정

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useEffect } from "react";
 import "./App.scss";
 import { useDispatch } from "react-redux";
 import { fetchMultiplePokemonById } from "./RTK/thunk";
-import { Link, Route, Routes } from "react-router-dom";
+import { Link, Route, Routes, useNavigate } from "react-router-dom";
 import Main from "./pages/Main";
 import Detail from "./pages/Detail";
 import Search from "./pages/Search";
 import Favorite from "./pages/Favorite";
 
 function App() {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -18,11 +19,14 @@ function App() {
   return (
     <>
       <h1 className="text-[40px] text-center">포켓몬 도감</h1>
-      <nav className="flex gap-[10px] justify-center">
+      <nav className="flex gap-[20px] justify-center">
         <Link to={"/"}>메인</Link>
-        <Link to={"detail/1"}>상세정보</Link>
-        <Link to={"search"}>검색</Link>
         <Link to={"favorite"}>찜목록</Link>
+        <span>🔍</span>
+        <input
+          onChange={(e) => navigate(`/search?pokemon=${e.target.value}`)}
+          className="w-[120px]  border-b border-[darkgray] px-2"
+        />
       </nav>
       <main className="flex flex-wrap gap-[20px] pt-[20px] justify-center">
         <Routes>
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,3 +1,8 @@
+import { useSearchParams } from "react-router-dom";
+
 export default function Search() {
+  const [searchParams] = useSearchParams();
+  const param = searchParams.get(`pokemon`);
+
   return <div>Search</div>;
 }
