d3736b85bc696cda9c58ccaee0a9f122cdd5597d
L <lmwoo0327@naver.com>
2025-06-12 16:59:40 +0900

    fetch #11. favoriteSlice 기능 추가

--- src/RTK/slice.js
+++ src/RTK/slice.js
@@ -16,9 +16,23 @@ export const pokemonSlice = createSlice({
       .addCase(fetchMultiplePokemonById.rejected, (state) => {
         state.loading = false;
       })
-      .addCase(fetchMultiplePokemonById.fulfilled, (state, aciton) => {
+      .addCase(fetchMultiplePokemonById.fulfilled, (state, action) => {
         state.loading = false;
-        state.data = aciton.payload;
+        state.data = action.payload;
       });
   }, // 비동기적 상태 변경
+});
+
+export const favoriteSlice = createSlice({
+  name: "favorite",
+  initialState: [1, 2, 3],
+  reducers: {
+    addToFavorite(state, action) {
+      state.push(action.payload.pokemonId);
+    },
+    removeFromFavorite(state, action) {
+      const index = state.indexOf(action.payload.pokemonId);
+      if (index !== -1) state.splice(index, 1);
+    },
+  },
 });
