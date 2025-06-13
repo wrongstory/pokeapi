972d89bcb1a23a68092768b006bc9d6b93169337
L <lmwoo0327@naver.com>
2025-06-13 14:08:44 +0900

    fetch #19. cart 컴포넌트를 memo 사용하여 리렌더링 방지

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import FavoriteButton from "./FavoriteButton";
+import { memo, useState } from "react";
 
 const CardContainer = styled.section`
   width: 150px;
@@ -20,15 +21,25 @@ const CardContainer = styled.section`
   }
 `;
 
-export const Card = ({ pokemon }) => {
+export const Card = memo(({ pokemon }) => {
+  const [isImageLoading, setIsImageLoading] = useState(true);
   const navigate = useNavigate();
   return (
     <CardContainer onClick={() => navigate(`/detail/${pokemon.id}`)}>
-      <img src={pokemon.front} />
+      {isImageLoading ? (
+        <div className="w-[120px] h-[120px] leading-[120px] text-center">
+          로딩중...
+        </div>
+      ) : null}
+      <img
+        onLoad={() => setIsImageLoading(false)}
+        src={pokemon.front}
+        style={{ display: isImageLoading ? "none" : "block" }}
+      />
       <div>
         {pokemon.name}
         <FavoriteButton pokemonId={pokemon.id} />
       </div>
     </CardContainer>
   );
-};
+});
