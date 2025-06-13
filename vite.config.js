a8db8ff984a6a325b64ecffd30225f4e488aaf23
L <lmwoo0327@naver.com>
2025-06-13 13:52:33 +0900

    fetch #17. node module 에서 필요 모듈만 가져올수 있게

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "docs",
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.indexOf("node_modules") !== -1) {
+            const module = id.split("node_modules/").pop().split("/")[0];
+            return `vendor-${module}`;
+          }
+        },
+      },
+    },
+  },
+});
